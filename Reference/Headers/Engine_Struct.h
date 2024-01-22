20e2a3eb30701818e703c4b611bdd7ade41d298d
26255621 <26255621@naver.com>
2024-01-22 23:26:26 +0900

    머지함
    낄

--- Reference/Headers/Engine_Struct.h
+++ Reference/Headers/Engine_Struct.h
@@ -60,7 +60,7 @@ namespace Engine
 	typedef struct tagSSAOValue {
 		float fIntensity = 3.f;
 		float fRadius = 0.05f;
-		float fScale = 5.f;
+		float fScale = 3.f;
 		float fBias = 0.25f;
 	}SSAO_DESC;
 
