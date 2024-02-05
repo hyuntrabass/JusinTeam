0eb13e7bb34f9c7db6676bebabfa0f65cd765e39
nachojo <kjwjj486@gmail.com>
2024-02-05 22:55:39 +0900

    스킬 알림창 만드는중

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -1325,6 +1325,10 @@ HRESULT CLoader::Load_GamePlay()
 	{
 		return E_FAIL;
 	}
+	if (FAILED(m_pGameInstance->Add_Prototype_GameObejct(TEXT("Prototype_GameObject_PopSkill"), CPop_Skill::Create(m_pDevice, m_pContext))))
+	{
+		return E_FAIL;
+	}
 	
 
 #pragma endregion
