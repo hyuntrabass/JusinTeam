93079d86bcd86877dcd673e7d09abf7f8a8af24c
PTHJusin <gksrnr476@naver.com>
2024-02-08 00:52:20 +0900

    그로아 컷신 사운드 작업함

--- Client/Private/Groar_Boss.cpp
+++ Client/Private/Groar_Boss.cpp
@@ -10,6 +10,7 @@
 #include "TextButtonColor.h"
 #include "Trigger_Manager.h"
 #include "HitEffect.h"
+#include "FadeBox.h"
 
 const _float CGroar_Boss::m_fChaseRange = 10.f;
 const _float CGroar_Boss::m_fAttackRange = 6.f;
@@ -94,28 +95,6 @@ void CGroar_Boss::Tick(_float fTimeDelta)
 			//pPlayerTransform->Set_Position(vPlayerPos);
 
 			m_bChangePos = true;
-		}
-	}
-
-	//if (m_pGameInstance->Key_Down(DIK_G)) // 자살 시작(보스로 변하는 타이밍)
-	if (CTrigger_Manager::Get_Instance()->Is_Coll_BossTrigger() == true || m_pGameInstance->Key_Down(DIK_G))
-	{
-		m_eCurState = STATE_SCENE01;
-		//m_eCurState = STATE_BOSS;
-		//m_eBossCurState = BOSS_STATE_ROAR;
-
-		if (m_pHpBoss == nullptr)
-		{
-			CHPBoss::HPBOSS_DESC Desc{};
-			Desc.strName = L"Groar";
-			Desc.eLevelID = LEVEL_STATIC;
-			Desc.iMaxHp = m_iHP;
-
-			m_pHpBoss = (CHPBoss*)m_pGameInstance->Clone_Object(TEXT("Prototype_GameObject_HPBoss"), &Desc);
-			if (not m_pHpBoss)
-			{
-				return;
-			}
 		}
 	}
 
@@ -960,6 +939,9 @@ HRESULT CGroar_Boss::Init_Dialog()
 	m_TalkSounds.push_back(TEXT("10054_1_StartTalk_1_cut"));
 	m_TalkSounds.push_back(TEXT("10056_1_StartTalk_1"));
 	m_TalkSounds.push_back(TEXT("10056_1_StartTalk_2"));
+	m_TalkSounds.push_back(TEXT("10056_2_InformTalk"));
+	m_TalkSounds.push_back(TEXT("10058_2_InformTalk_1"));
+	m_TalkSounds.push_back(TEXT("10058_2_InformTalk_2"));
 
 	return S_OK;
 }
@@ -1125,14 +1107,7 @@ void CGroar_Boss::NPC_Tick(_float fTimeDelta)
 	m_pLine->Tick(fTimeDelta);
 	m_pBackGround->Tick(fTimeDelta);
 	m_pTransformCom->Gravity(fTimeDelta);
-	//사운드 채널 갱신 / 그로아 사운드 나오는 도중에 사운드 넘어가기 위해
-	if (m_iSoundChannel != -1)
-	{
-		if (not m_pGameInstance->Get_IsPlayingSound(m_iSoundChannel))
-		{
-			m_iSoundChannel = -1;
-		}
-	}
+
 	if (m_bTalking == true)
 	{
 		m_pArrow->Tick(fTimeDelta);
@@ -1160,7 +1135,52 @@ void CGroar_Boss::NPC_LateTick(_float fTimeDelta)
 		}
 	}
 
+	//if (m_pGameInstance->Key_Down(DIK_G)) // 자살 시작(보스로 변하는 타이밍)
+	if (CTrigger_Manager::Get_Instance()->Is_Coll_BossTrigger() == true || m_pGameInstance->Key_Down(DIK_G))
+	{
+		if (m_TalkSounds.size() != 0 && m_iSoundChannel == -1)
+		{
+			m_iSoundChannel = m_pGameInstance->Play_Sound(m_TalkSounds.front());
+			m_TalkSounds.pop_front();
+		}
+	}
+	if (m_iSoundChannel == -1 && m_TalkSounds.size() == 0)
+	{
+		m_eCurState = STATE_SCENE01;
+		//m_eCurState = STATE_BOSS;
+		//m_eBossCurState = BOSS_STATE_ROAR;
 
+		if (m_pHpBoss == nullptr)
+		{
+			CHPBoss::HPBOSS_DESC Desc{};
+			Desc.strName = L"Groar";
+			Desc.eLevelID = LEVEL_STATIC;
+			Desc.iMaxHp = m_iHP;
+
+			m_pHpBoss = (CHPBoss*)m_pGameInstance->Clone_Object(TEXT("Prototype_GameObject_HPBoss"), &Desc);
+			if (not m_pHpBoss)
+			{
+				return;
+			}
+
+			CFadeBox::FADE_DESC FadeBoxDesc = {};
+			FadeBoxDesc.eState = CFadeBox::FADEOUT;
+			FadeBoxDesc.fDuration = 1.f;
+
+			if (FAILED(m_pGameInstance->Add_Layer(LEVEL_STATIC, TEXT("Layer_UI"), TEXT("Prototype_GameObject_FadeBox"), &FadeBoxDesc)))
+			{
+				return;
+			}
+		}
+	}
+	//사운드 채널 갱신 / 그로아 사운드 나오는 도중에 사운드 넘어가기 위해
+	if (m_iSoundChannel != -1)
+	{
+		if (not m_pGameInstance->Get_IsPlayingSound(m_iSoundChannel))
+		{
+			m_iSoundChannel = -1;
+		}
+	}
 }
 
 void CGroar_Boss::Set_Text(GROAR_NPCSTATE eState)
