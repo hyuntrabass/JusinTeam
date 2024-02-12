d82222709154c86d26bd5b251712ac2fd09f0541
PTHJusin <gksrnr476@naver.com>
2024-02-12 14:05:04 +0900

    필드에서 스킬북 갔다오면 사운드 고장나는거 수정

--- Client/Private/Trigger_Manager.cpp
+++ Client/Private/Trigger_Manager.cpp
@@ -37,6 +37,7 @@ void CTrigger_Manager::Tick(_float fTimeDelta)
 				m_strFilePath = L"../Bin/Data/Village_CutScene.dat";
 				m_pGameInstance->Set_CameraModeIndex(CM_CUTSCENE);
 				CUI_Manager::Get_Instance()->Set_Symbol(CSymbol::VILLAGE);
+				m_isInVillage = true;
 				iter->Set_Limited(false);
 			}
 			else if (iter->Get_TriggerType() == FRONTDOOR_IN_TRIGGER)
@@ -45,10 +46,17 @@ void CTrigger_Manager::Tick(_float fTimeDelta)
 				if (iter->Get_Limited() == false)
 				{
 					CUI_Manager::Get_Instance()->Set_Symbol(CSymbol::FIELDEAST);
-					m_pGameInstance->FadeoutSound(0, fTimeDelta);
-					m_iSoundChannel = m_pGameInstance->Play_Sound(TEXT("Midgard_Field"), 0.3f, true);
-					m_pGameInstance->FadeinSound(m_iSoundChannel, fTimeDelta);
+					if (m_isInVillage)
+					{
+						m_pGameInstance->FadeoutSound(0, fTimeDelta, 1.f, false);
+					}
 					iter->Set_Limited(true);
+				}
+				if (not m_pGameInstance->Get_IsPlayingSound(0))
+				{
+					m_pGameInstance->PlayBGM(TEXT("Midgard_Field"));
+					m_pGameInstance->FadeinSound(0, fTimeDelta);
+					m_isInVillage = false;
 				}
 			}
 			else if (iter->Get_TriggerType() == FRONTDOOR_OUT_TRIGGER)
@@ -57,13 +65,17 @@ void CTrigger_Manager::Tick(_float fTimeDelta)
 				if (iter->Get_Limited() == false)
 				{
 					CUI_Manager::Get_Instance()->Set_Symbol(CSymbol::VILLAGE);
-					if (m_iSoundChannel != -1)
+					if (not m_isInVillage)
 					{
-						m_pGameInstance->FadeoutSound(m_iSoundChannel, fTimeDelta, 1.f, false);
-						m_iSoundChannel = -1;
+						m_pGameInstance->FadeoutSound(0, fTimeDelta, 1.f, false);
 					}
-					m_pGameInstance->FadeinSound(0, fTimeDelta);
 					iter->Set_Limited(true);
+				}
+				if (not m_pGameInstance->Get_IsPlayingSound(0))
+				{
+					m_pGameInstance->PlayBGM(TEXT("BGM_1st_Village"));
+					m_pGameInstance->FadeinSound(0, fTimeDelta);
+					m_isInVillage = true;
 				}
 			}
 			else if (iter->Get_TriggerType() == BACKDOOR_IN_TRIGGER)
@@ -72,10 +84,17 @@ void CTrigger_Manager::Tick(_float fTimeDelta)
 				if (iter->Get_Limited() == false)
 				{
 					CUI_Manager::Get_Instance()->Set_Symbol(CSymbol::FIELDSOUTH);
-					m_pGameInstance->FadeoutSound(0, fTimeDelta);
-					m_iSoundChannel = m_pGameInstance->Play_Sound(TEXT("Midgard_Field"), 0.3f, true);
-					m_pGameInstance->FadeinSound(m_iSoundChannel, fTimeDelta);
+					if (m_isInVillage)
+					{
+						m_pGameInstance->FadeoutSound(0, fTimeDelta, 1.f, false);
+					}
 					iter->Set_Limited(true);
+				}
+				if (not m_pGameInstance->Get_IsPlayingSound(0))
+				{
+					m_pGameInstance->PlayBGM(TEXT("Midgard_Field"));
+					m_pGameInstance->FadeinSound(0, fTimeDelta);
+					m_isInVillage = false;
 				}
 			}
 			else if (iter->Get_TriggerType() == BACKDOOR_OUT_TRIGGER)
@@ -84,13 +103,17 @@ void CTrigger_Manager::Tick(_float fTimeDelta)
 				if (iter->Get_Limited() == false)
 				{
 					CUI_Manager::Get_Instance()->Set_Symbol(CSymbol::VILLAGE);
-					if (m_iSoundChannel != -1)
+					if (not m_isInVillage)
 					{
-						m_pGameInstance->FadeoutSound(m_iSoundChannel, fTimeDelta, 1.f, false);
-						m_iSoundChannel = -1;
+						m_pGameInstance->FadeoutSound(0, fTimeDelta, 1.f, false);
 					}
-					m_pGameInstance->FadeinSound(0, fTimeDelta);
 					iter->Set_Limited(true);
+				}
+				if (not m_pGameInstance->Get_IsPlayingSound(0))
+				{
+					m_pGameInstance->PlayBGM(TEXT("BGM_1st_Village"));
+					m_pGameInstance->FadeinSound(0, fTimeDelta);
+					m_isInVillage = true;
 				}
 			}
 			else if (iter->Get_TriggerType() == BOSS_TRIGGER && iter->Get_Limited() == true)
@@ -143,8 +166,6 @@ void CTrigger_Manager::Tick(_float fTimeDelta)
 			}
 		}
 	}
-
-
 }
 
 void CTrigger_Manager::Limited_CutScene(_bool isLimited)
