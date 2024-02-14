02cd780263557a172e866222d00936b566dbe910
26255621 <26255621@naver.com>
2024-02-14 16:34:50 +0900

    실수로 잘못땡김

--- Engine/Private/GameInstance.cpp
+++ Engine/Private/GameInstance.cpp
@@ -146,6 +146,10 @@ void CGameInstance::Tick_Engine(_float fTimeDelta)
 		}
 	}
 
+	if (Key_Down(DIK_F5, InputChannel::Engine)) {
+		m_bTurnOnShadow = m_bTurnOnShadow;
+	}
+
 	if (Is_Playing_Video())
 	{
 		return;
